079d5a1e572b6bfc31adb1a910080aabdae5180e
waiyantun <waiyan.web@gmail.com>
2022-07-12 20:45:02 +0630

    5 stap of video

--- main.py
+++ main.py
@@ -15,7 +15,8 @@ pygame.display.set_icon(icon)
 playerImage = pygame.image.load('ship.png')
 playerX  = 470
 playerY  = 500
-playerImg_Change = 0
+playerX_change = 0
+playerY_change = 0
 
 #draw ship on screen
 def player(x,y):
@@ -29,13 +30,30 @@ while running:
 
     #Background color
     screen.fill((80,50,100))
-    playerY -= 0.1
+    
     
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
         
+        #if keystroke is pressed check whether its right or left
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                playerX_change = -0.4
+            if event.key == pygame.K_RIGHT:
+                playerX_change = 0.4
+            if event.key == pygame.K_UP:
+                playerY_change = -0.4
+            if event.key == pygame.K_DOWN:
+                playerY_change =  0.4
 
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                playerX_change = 0
+            if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                playerY_change = 0
+
+    playerY += playerY_change
+    playerX += playerX_change
     player(playerX,playerY)
     pygame.display.update()
