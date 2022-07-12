6b204e7de94316d8934e91e4dd48e974e3a52b0e
waiyantun <waiyan.web@gmail.com>
2022-07-12 18:09:37 +0630

    adding ship image to our game

--- main.py
+++ main.py
@@ -8,16 +8,32 @@ screen = pygame.display.set_mode((1000,600))
 
 #Title and icon
 pygame.display.set_caption("Alien Invasion")
-icon = pygame.image.load('spaceship.png')
+icon = pygame.image.load('icon.png')
 pygame.display.set_icon(icon)
+
+#player
+playerImage = pygame.image.load('ship.png')
+playerX  = 470
+playerY  = 500
+
+#draw ship on screen
+def player():
+    """Something"""
+    screen.blit(playerImage,(playerX,playerY))
+
 
 #Game Loop
 running = True
 while running:
+
+    #Background color
+    screen.fill((80,50,100))
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-        #Background color
-        screen.fill((250,200,255))
-        pygame.display.update()
+        
+
+    player()
+    pygame.display.update()
